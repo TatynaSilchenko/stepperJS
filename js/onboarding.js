5e64d67303c9faa29ef15ee1f76336a3f67bd92b
tatiana silchenko <tatiana.s@abiatec.com>
2023-09-16 15:43:24 +0300

    adaptive for stepper

--- js/onboarding.js
+++ js/onboarding.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    let StepNames =  [
+  let StepNames =  [
         {"title" : 'Choose your industry'},
         {"title" : 'Which versions of the product do you wish to build at this stage?'},
         {"title" : 'What type of platform do you need?'},
@@ -11,49 +11,47 @@ $(document).ready(function(){
         'subtitles': ['Real-time communication ', 'User roles', 'Social features', 'Geolocation services', 'Payments and purchasing ', 'Calendar']
       },
         {"title" : 'How much time do you have?'},
-                    ]
-    let current_fs, next_fs; //fieldsets
-    let opacity;
-    let current = 1;
-    let steps = $(".top-fieldset").length;
-    
-    setProgressBar(current);
-    
-    $(".next.nextBtn").click(function(){
-    renderNextStep ();
-    });
-    
-    $(".previous").click(function(){
-    renderNextStep (true);
-    });
+                  ]
+  let current_fs, next_fs; //fieldsets
+  let opacity;
+  let current = 1;
+  let steps = $(".top-fieldset").length;
 
-    $("#startStepper").click(function(){
+  let counterActiveImg = 0;
+  let counterActiveArrow = 0;
+
+  setProgressBar(current);
+
+  $(".next.nextBtn").click(function(){
+    renderNextStep ();
+  });
+
+  $(".previous").click(function(){
+    renderNextStep (true);
+  }); 
+  $("#startStepper").click(function(){
       $("#onboarding").hide();
       $("#onboardingCalc").show();
-    })
-    
-    $(".jumpBtn").click(function(){
-      $('#onboardingCalc').hide() ; 
-      $('#onboardingThankyou').show() ; 
+      $("body").css("padding","0 calc(20px - (100vw - 100%)) 0 0");
+  })
 
-    })
-
-
-
-    function setProgressBar(curStep){
+  $(".jumpBtn").click(function(){
+    $('#onboardingCalc').hide() ; 
+    $('#onboardingThankyou').show();
+    $("body").css("padding","0");   
+  })  
+  function setProgressBar(curStep){
       let percent = parseFloat(100 / steps) * curStep;
       percent = percent.toFixed();
       $(".progress-bar")
       .css("width",percent+"%")
-      }
-
-    const checkIsPreviousStep = (id) => {
-         (id !== "step1") 
-       ? $("#prevBtn").css("display",'flex')
-       : $("#prevBtn").hide()
-    }
-
-    const checkID = (id, isPrev = false) => {
+    } 
+  const checkIsPreviousStep = (id) => {
+       (id !== "step1") 
+     ? $("#prevBtn").css("display",'flex')
+     : $("#prevBtn").hide()
+  } 
+  const checkID = (id, isPrev = false) => {
       let subStepNumber;
       let nextStep;
       let parts = id.split("_");
@@ -71,52 +69,44 @@ $(document).ready(function(){
       : (+numberID + 1);
 
       return [numberID, subStepNumber, nextStep, nextStepNumber];
-    }
-
-    const changeId = (id, index) => {
+  } 
+  const changeId = (id, index) => {
       return `${id}_${index + 1}`
-    }
+  }
+  const renderNextStep = ( isPrev = false ) => {
+  current_fs = $('.formFieldset-wrapper__main-info .fieldset.active');
+  let current_fs__id = current_fs.attr('id');
+  let [ numberID, subStepNumber, nextStep, nextStepNumber ] = checkID(current_fs__id, isPrev);
+  next_fs =  $(`#${nextStep}`);
+  checkIsPreviousStep(nextStep);
 
-    const renderNextStep = ( isPrev = false ) => {
-      current_fs = $('.formFieldset-wrapper__main-info .fieldset.active');
-      let current_fs__id = current_fs.attr('id');
-      let [ numberID, subStepNumber, nextStep, nextStepNumber ] = checkID(current_fs__id, isPrev);
-      next_fs =  $(`#${nextStep}`);
-      checkIsPreviousStep(nextStep);
-      if (nextStep == "step9") {
-        $('.nextBtn').hide(); 
-        $('.jumpBtn').show(); 
-      } 
-      showNewStep(current_fs, next_fs, nextStepNumber);
+  if ($(`#${nextStep} input[type='radio']:checked`).length) {
+    let checkedRadio = $(`#${nextStep} input[type='radio']:checked`);
+    let checkedRadioId = checkedRadio.attr('id');
+    let checkedRadioName = checkedRadio.attr('name');
 
-      setHeaderTitle(nextStepNumber);
-         
-      if (subStepNumber) return;
 
-      isPrev ? setProgressBar(--current) : setProgressBar(++current);
-    }
+    !$(`#selectedOptions #${checkedRadioId}Tag`).length &&   renderNewOption(checkedRadio, checkedRadioName);
+    console.log( $(` #${checkedRadioId}`))
+  }
+  if (nextStep == "step9") {
+    $('.nextBtn').hide(); 
+    $('.jumpBtn').show(); 
+  } 
+  showNewStep(current_fs, next_fs, nextStepNumber);
+  setHeaderTitle(nextStepNumber);
+     
+  if (subStepNumber) return;
+  isPrev ? setProgressBar(--current) : setProgressBar(++current);
+  }
+  const showNewStep = (current_fs, next_fs, nextStepNumber ) => {
+  current_fs.removeClass('active');
+  next_fs.addClass('active');
+  let next_fs_id = next_fs.attr('id');
+  next_fs.show();
+  let nxtStepRadio =  $(`#${next_fs_id} input[type='radio']:checked`);
 
-    const showNewStep = (current_fs, next_fs, nextStepNumber ) => {
-      current_fs.removeClass('active');
-      next_fs.addClass('active');
-      let next_fs_id = next_fs.attr('id');
-      next_fs.show();
-
-      let nxtStepRadio =  $(`#${next_fs_id} input[type='radio']:checked`);
-
-      // if (nxtStepRadio.length) {
-      //   let nxtStepRadioName =  $(`#${next_fs_id} input[type='radio']:checked`).attr("name");
- 
-      //   $(`[data-input=${nxtStepRadioName}]`).remove();
-
-      //   renderNewOption(nxtStepRadio, nxtStepRadioName);
- 
-      //  addOptionToList(nxtStepRadioName);
-      //  } else {
-
-      //  }
-
-      switch (nextStepNumber) {
+  switch (nextStepNumber) {
     
         case 2 :
           addOptionToList('productVersion');
@@ -127,9 +117,8 @@ $(document).ready(function(){
         case 9 : 
         addOptionToList('time');
         break;
-      }
-
-      current_fs.animate({opacity: 0}, {
+  }
+  current_fs.animate({opacity: 0}, {
         step: function(now) {
         // for making fielset appear animation
         opacity = 1 - now;
@@ -141,37 +130,37 @@ $(document).ready(function(){
         next_fs.css({'opacity': opacity, 'display': 'grid'});
         },
         duration: 500
-        });
-    }
+    });
+  }
 
-    const setHeaderTitle = (number) => {
+  const setHeaderTitle = (number) => {
        return  $("#stepName").html(StepNames[number - 1]["title"])
-    }
-
-     const addOptionToList = (name) => {
+  }
+   const addOptionToList = (name) => {
       $(`input[type='radio'][name=${name}]`).on("change", function() {
         // Находим выбранную радиокнопку среди радиокнопок с атрибутом name="industry"
         let selectedRadioButton = $(`input[type='radio'][name=${name}]:checked`);
-      
-        // Удаляем остальные радиокнопки с атрибутом name="industry"
+        let id = selectedRadioButton.attr("id");
+    
         $(`[data-input=${name}]`).remove();
-      
-        // Добавляем значение (или текст) в элемент с ID "selectedOptions"
 
         renderNewOption(selectedRadioButton, name)
+
+        if (id.indexOf("other") !== -1) {
+          selectedRadioButton.closest('.fieldset').find('.addition-input').show();
+        } else {
+          selectedRadioButton.closest('.fieldset').find('.addition-input').hide();
+        }
       });
 
-    }
-
-    const renderNewOption = (el, name ) => {
+  }
+  const renderNewOption = (el, name ) => {
       let elementId =  el.attr('id');
       let selectedText = el.val();
      return  $("#selectedOptions").append(`<div class="fieldset__payment__tag" data-input=${name ? name : selectedText} id="${elementId}Tag">${selectedText}</>`)
-    }
-
-     addOptionToList('industry');
-
-     $("input[type='checkbox']").on("change", function() {
+  }
+   addOptionToList('industry');
+   $("input[type='checkbox']").on("change", function() {
       let selectedCheckboxID = $(this).attr('id');
       if ($(this).is(":checked")) {
         renderNewOption($(this), $(this));
@@ -180,9 +169,8 @@ $(document).ready(function(){
         let newCon =  $(`#${selectedCheckboxID}Tag`)
         newCon.remove();
       }
-    });
-
-    $(".multiStep .checkbox__input").on("change", function(e) {
+  });
+  $(".multiStep .checkbox__input").on("change", function(e) {
       
       if ($(this).is(":checked")) {
        let checkboxes = $(".add-checkbox");
@@ -194,17 +182,17 @@ $(document).ready(function(){
 
        let nxtStepRadio =  $(`#${newId} input[type='radio']:checked`);
 
-      //  if (nxtStepRadio.length) {
-      //   let nxtStepRadioName =  $(`#${newId} input[type='radio']:checked`).attr("name");
+       if (nxtStepRadio.length) {
+        let nxtStepRadioName =  $(`#${newId} input[type='radio']:checked`).attr("name");
  
-      //   $(`[data-input=${nxtStepRadioName}]`).remove();
+        $(`[data-input=${nxtStepRadioName}]`).remove();
 
-      //   renderNewOption(nxtStepRadio, nxtStepRadioName);
+        renderNewOption(nxtStepRadio, nxtStepRadioName);
  
-      //  addOptionToList(nxtStepRadioName);
-      //  } else {
+       addOptionToList(nxtStepRadioName);
+       } else {
 
-      //  }
+       }
 
 
 
@@ -213,11 +201,10 @@ $(document).ready(function(){
        $("#stepName").html(StepNames[numberID - 1]["subtitles"][index])
     showNewStep(closestMultiStep, nextStep);
       }
-    });
+  });
+  //change state of checkbox
 
-    //change state of checkbox
-
-const changeCheckbox = () => {
+  const changeCheckbox = () => {
 
   let checkInput = document.querySelectorAll('.checkmark');
   if (checkInput) {
@@ -236,11 +223,11 @@ const changeCheckbox = () => {
       })
     })
   }
-}
+  }
 
-changeCheckbox();
+  changeCheckbox();
 
-let projectTeam = new Swiper('.caseSwiperTeamOnboard', {
+  let projectTeam = new Swiper('.caseSwiperTeamOnboard', {
   spaceBetween: 10,
   slidesPerView: 1,
   infinite: true,
@@ -267,8 +254,24 @@ let projectTeam = new Swiper('.caseSwiperTeamOnboard', {
 
     },
   },
-});
+  });
 
     
+function toggleActiveClass() {
+  var bgCasesImgs = $(".bg-cases-img");
+  var showArrows = $(".show-arrow");
+
+  bgCasesImgs.removeClass("active");
+  showArrows.removeClass("active");
+
+  bgCasesImgs.eq(counter).addClass("active");
+  showArrows.eq(counter).addClass("active");
+
+  counter = (counter + 1) % bgCasesImgs.length;
+}
+
+var counter = 0;
+
+setInterval(toggleActiveClass, 1400);
     })
 
